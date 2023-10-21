06e3599c5b439bf7018522fb449691b79e77ecf2
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2023-10-21 14:48:40 +0900

    feat: member login proccess

--- controller/memberController.js
+++ controller/memberController.js
@@ -8,19 +8,29 @@ memberController.signup = async (req, res) =>{
     try{
        console.log('POST: cont/signup');
        const data = req.body;
-       console.log('body:::', req.body);
        const member = new Member();
        const new_member = await member.signupData(data)
        
 
-       res.send('done');
+       res.json({state: 'succed', data : new_member})
     } catch (err) {
-      console.log(`ERROR, cont/signup,${err.message}`);
+      console.log(`ERROR, cont/signup, ${err.message}`);
+      res.json({state : 'fail', message : err.message})
     }
 }
-memberController.login = (req, res) =>{
-    console.log('POST cont.login');
-    res.send('login sahifasidasiz');
+memberController.login = async (req, res) =>{
+    try{
+        console.log('POST: cont/login');
+        const data = req.body;
+        const member = new Member();
+        result = await member.loginData(data)
+        
+ 
+        res.json({state: 'succed', data : result})
+     } catch (err) {
+       console.log(`ERROR, cont/signup,${err.message}`);
+       res.json({state : 'fail', message : err.message})
+     }
 }
 memberController.logout = (req, res) =>{
     console.log('GET cont.logout');
--- models/Member.js
+++ models/Member.js
@@ -1,5 +1,6 @@
 const MemberModel = require('../scheme/member.module.js')
-const Definer = require('../lib/mistake.js')
+const Definer = require('../lib/mistake.js');
+const assert = require('assert')
 
 
 class Member {
@@ -10,15 +11,39 @@ class Member {
     async signupData(input) {
         try{
              const new_member  = new this.memberModel(input) ;
+             let result;
             try{
-              const result  = await new_member.save();
+               result  = await new_member.save();
             } catch(mongo_err) {
                 console.log(mongo_err);
                throw new Error(Definer.auth_err1)
             }
         
-            result.mp_password = '';
+            result.mb_password = "";
          return result
+        } catch (err) {
+           throw err
+        }
+    }
+
+
+    async loginData(input) {
+        try{
+            const member = await  this.memberModel
+            .findOne({mb_nick: input.mb_nick}, {mb_nick: 1, mb_password:1 })
+            .exec();
+
+        assert.ok(member, Definer.auth_err3)
+        
+        const isMatch = input.mb_password === member.mb_password;
+        assert.ok(isMatch, Definer.auth_err4)
+
+
+        return await this.memberModel
+         .findOne({
+            mb_nick: input.mb_nick
+         })
+         .exec()
         } catch (err) {
            throw err
         }
