4eb9dec9f8c8217c7b8a7ee94fc9f1e5c3063f11
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2023-10-28 13:08:48 +0900

    feat: restaurant user getting all restauraant products

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,20 @@ class Product {
       this.productModel = ProductModel
     }
 
+    async getAllProductsDataResto(member) {
+      try{
+        member._id = shapeIntoMongooseObjectId(member._id)
+        const result = await this.productModel.find({
+          restaurant_mb_id : member._id,
+        })
+        assert.ok(result, Definer.general_err1)
+        console.log('sassa', result);
+        return result
+      }catch(err){
+        throw err
+      }
+    }
+
      async addNewProductData(data, member){
       try{
         data.restaurant_mb_id = shapeIntoMongooseObjectId(member._id);
