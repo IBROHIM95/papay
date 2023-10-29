323ce9063ad4723d206502f1615a876dc2ee74cc
IBROHIM95 <shodibek.tokhirov95@gmail.com>
2023-10-29 16:48:47 +0900

    fix: modify logic of login proccess

--- controller/restaurantController.js
+++ controller/restaurantController.js
@@ -68,14 +68,16 @@ restaurantController.getLoginMyRestaurant = async(req,res) =>{
 
 restaurantController.loginProccess = async (req, res) =>{
     try{
-        console.log('POST: cont/login-page');
+        console.log('POST: cont/loginProccess');
         const data = req.body;
-         member = new Member();
+        member = new Member();
         result = await member.loginData(data)
         
         req.session.member = result;
         req.session.save(function() {
-          res.redirect('/resto/products/menu')
+          result.mb_type ==='ADMIN' 
+          ? res.redirect('/resto/all-restaurant')   
+          : res.redirect('/resto/products/menu');
         })
  
         
@@ -98,18 +100,6 @@ restaurantController.validateAuthRestaurant = (req, res, next) => {
       message: "Only authenticated members with restaurant type are allowed",
     });
 };
-
-// restaurantController.validateAuthRestaurant = (req,res,next) => {
-//   if(req.session?.member?.mb_type === 'RESTAURANT'){
-//     req.member = req.session.member;
-//     next();
-//   }else {
-//     res.json({
-//       state : 'fail',
-//       error : 'only authenticated members with restaurant type  '
-//     })
-//   }
-// }
 
 restaurantController.checkSessions = (req, res) =>{
     if(req.session?.member) {
